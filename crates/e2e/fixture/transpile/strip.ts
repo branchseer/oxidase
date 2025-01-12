cd84135edc01f2a900e42e6f7e0dd90ea3244be9
branchseer <dk4rest@gmail.com>
2025-01-12 09:46:06 +0800

    add babel test fixtures & strip test case

---
+++ crates/e2e/fixture/transpile/strip.ts
@@ -0,0 +1,19 @@
+class A {
+    constructor(public a) {
+        // rewind
+        function aa() {
+            foo<{ a: () => (x<T>) + y}>(1);
+        }
+
+        function bb() {
+            foo<{ a: () => (x<T>) & y}>(1);
+        }
+
+        super();
+
+        function cc() {
+            const bar = (a = () => 1 as number);
+        }
+        const baz = (a = () => 1 as number) => 1;
+    }
+}
