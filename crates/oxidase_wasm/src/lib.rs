3ea9283d2fd8af7fc28bdcdc7e43d3f2cf56c2c7
branchseer <dk4rest@gmail.com>
2025-01-14 22:36:07 +0800

    update wasm

--- crates/oxidase_wasm/src/lib.rs
+++ crates/oxidase_wasm/src/lib.rs
@@ -1,13 +1,19 @@
 mod utils;
 
+use std::cell::RefCell;
+
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+thread_local! {
+    static ALLOCATOR: RefCell<oxidase::Allocator> = Default::default();
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, oxidase-wasm!");
+pub fn transpile(mut source: String) -> String {
+    console_error_panic_hook::set_once();
+    ALLOCATOR.with_borrow_mut(|allocator| {
+        let ret = oxidase::transpile(allocator, oxidase::SourceType::ts(), &mut source);  
+        allocator.reset();
+        source
+    })
 }
