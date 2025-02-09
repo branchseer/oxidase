aa7d6b84fd60a0ae5ebc7c8fb8f99107d426d67c
branchseer <dk4rest@gmail.com>
2025-02-09 20:34:29 +0800

    update example

--- packages/playground/src/example.ts
+++ packages/playground/src/example.ts
@@ -1,34 +1,24 @@
-// Just like ts-blank-space and swc
 let a: string = "";
+interface A {
+
+}
 
 enum Foo {
   A = 1,
   B = A + 2,
-//^^^^^^^^^ Positions of enum member initializaitons are preserved.
 }
 
-// Enum merging is supported
+// Enum merging
 enum Foo {
-  World = A
+  C = A
 }
-
 
 namespace Bar {
   export const A = 1;
-//       ^^^^^^^^^^^ Positions of exported declarations are preserved.
 }
-
 
 class Baz {
-  
+  constructor(public a: string) {
+    console.log(a);
+  }
 }
-
-
-enum A { a }
-namespace B { export const b = 1 }
-class C { constructor(public c) { } }
-
-
-A<{ a: 1 & 2 }>(0)
-A<{ a: 1 + 2 }>(0)
-
